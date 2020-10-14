3f01507b1f14bb925137205299b7b2bc8fcd1f26
Dessaleng Ewart <61801025+Binteldata@users.noreply.github.com>
2020-10-14 12:36:11 +0200

    Add files via upload

---
+++ if_elif.py
@@ -0,0 +1,10 @@
+print(' enter your age ')
+
+age = int(input())
+
+if age < 18:
+    print("you can't enter")
+elif age > 18 and age < 21:
+    print("come on in!")
+elif age >=21:
+    print("aw shit come on in it's drinking time!")
