a1c301421134f37d93422e45e00a7614bdcbefff
Dessaleng Ewart <61801025+Binteldata@users.noreply.github.com>
2020-10-15 12:53:52 +0200

    Add files via upload

---
+++ for.py
@@ -0,0 +1,21 @@
+for i in range(0, 3):
+    print(i)
+
+for a in range(0, 3):
+    print('...hello')
+
+my_list = [1, 2, 3, 4, 5]
+
+for i in my_list:
+    print(i)
+    print('-hut')
+
+for i in range(0, 10, 2):
+    print(i)
+    print('>hello')
+
+string = 'hi '
+
+for i in string:
+    print(i)
+    print('hi')
