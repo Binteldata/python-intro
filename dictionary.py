c3d6c3ad1ffbe353b7f0f220489071fdb1b98631
Dessaleng Ewart <61801025+Binteldata@users.noreply.github.com>
2020-11-03 11:27:10 +0100

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,34 @@
+# my_dict = {'name' : 'john', 'age' : 33, 'color' : 'red'};print(my_dict)
+# print(my_dict['name'])
+# print(my_dict['age'])
+# print(my_dict['color'])
+
+# test = {'x_position' : 0, 'y_position' : 25}
+# print('The locaction is ', test['x_position'], test['y_position'])
+# test['speed'] = 80
+# print(test)
+
+test = {
+    'john' : 'python',
+    'paul' : 'c',
+    'jack' : 'c++',
+    'joey' : 'java',
+    'my_list' : {'basketball' : 'Jordan', 'football' : 'Rice', 'entertainment' : 'jackson'}
+}
+
+test2 = {
+    'a' : 1,
+    'b' : 2,
+    'c' : 3,
+    }
+print(test,test2)
+
+# del test['john']
+# print(test)
+
+test['john'] = 'python3'
+test['speed'] = 80
+test2['a'] = 11
+test2['d'] = 'dictionary'
+print(test,test2)
+
